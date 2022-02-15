0278087fdb1e98cb4f14215fef73cf5e638ca57e
AnonymousCoderPar <98502720+AnonymousCoderPar@users.noreply.github.com>
2022-02-14 19:49:51 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+Webcam.set({
+    width:350,
+    height:300,
+    image_format: 'png',
+    png_quality:90
+    });
+    
+    camera = document.getElementById("camera");
+
+    Webcam.attach('#camera');
+
+    function take_snapshot() {
+        Webcam.snap(function(data_uri){
+document.getElementById("result").innerHTML='<img id="captured_image" src="'+data_uri+'">';
+        });
+    }
+
+    function startClassification()
+{
+navigator.mediaDevices.getUserMedia({ audio: true});
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/mLeanyjKl/model.json', modelReady);
+}
+
+ 
+
+function modelLoaded(){
+console.log('Model Loaded');
+}
+
+function speak(){
+    var synth = window.speechSynthesis;
+    speak_data_1 = "The Prediction Is " + prediction_1;
+    var utterThis = new SpeechSynthesisUtterance(speak_data_1 + speak_data_2);
+    synth.speak(utterThis);
+}
